a324134724b6a7e8ffb0e5b0aaa0d69fb41ea1b5
quangqv <quangqv@vng.com.vn>
2025-10-26 11:05:25 +0700

    add Material UI and Emotion dependencies, integrate Roboto font, and update layout with theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter"
+import { ThemeProvider } from "@mui/material/styles"
+import theme from "../theme"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +32,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   )
