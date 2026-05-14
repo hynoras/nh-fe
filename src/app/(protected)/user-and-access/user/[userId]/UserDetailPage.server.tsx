0b1514f31e3ac42d04bbec2fa81793b7da24ffab
Quách Vinh Quang <hynoras1@gmail.com>
2026-05-14 20:29:27 +0700

    feat(user-detail): split UserDetailPage into client and server page

---
+++ src/app/(protected)/user-and-access/user/[userId]/UserDetailPage.server.tsx
@@ -0,0 +1,18 @@
+import State from "components/state"
+import { checkPermissionServer } from "service/permission.server"
+import { PermissionCode } from "../../role/_const/permission"
+import UserDetailPageClient from "./UserDetailPage.client"
+
+const UserDetailPageServer = async () => {
+  const result = await checkPermissionServer(PermissionCode.USER_MANAGE)
+
+  if (!result.authorized) {
+    return (
+      <State.Forbidden description="Only users with permission to manage users can access this page." />
+    )
+  }
+
+  return <UserDetailPageClient />
+}
+
+export default UserDetailPageServer
