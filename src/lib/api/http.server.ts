582cfaa589973c37239f6dc8adb51960a7597daa
Quách Vinh Quang <hynoras1@gmail.com>
2026-05-14 15:41:41 +0700

    feat(api): split ky instance into client and server instance

---
+++ src/lib/api/http.server.ts
@@ -0,0 +1,14 @@
+import ky from "ky"
+import { cookies } from "next/headers"
+
+export const serverHttp = ky.create({
+  prefixUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
+  hooks: {
+    beforeRequest: [
+      async (req) => {
+        const cookieStore = await cookies()
+        req.headers.set("Cookie", cookieStore.toString())
+      }
+    ]
+  }
+})
